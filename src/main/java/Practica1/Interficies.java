460168ae6e1368df92bfbd2d798e3612984cf534
bertuviri <bertagv5@gmail.com>
2024-09-17 12:45:10 +0200

    Hola

---
+++ src/main/java/Practica1/Interficies.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Practica1;
+
+/**
+ *
+ * @author 6003171
+ */
+public class Interficies {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
