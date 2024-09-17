0e76b8705b6118a2d43b4b1e6409e25b039b9460
berta <berta@Berta>
2024-09-17 17:08:12 +0200

    Interficies

---
+++ src/main/java/com/mycompany/formulari1/PrimerFormulari.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formulari1;
+
+/**
+ *
+ * @author berta
+ */
+public class PrimerFormulari {
+
+    public static void main(String[] args) {
+        new Formulari().setVisible(true);
+    }
+}
